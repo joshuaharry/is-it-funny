d7ca344908c39cd1931b19911e8ff4aa0270130c
joshuaharry <84721070+joshuaharry@users.noreply.github.com>
2021-11-21 15:23:54 -0600

    Save heuristic results

---
+++ code/gen_heuristic_params.py
@@ -0,0 +1,81 @@
+from typing import List, Any
+from dataclasses import dataclass
+from heuristic import count_adult_slang, count_antonyms, count_sounds
+
+TRAINING_SIZE = 1000
+
+
+@dataclass
+class Sample:
+    samples: List[List[int]]
+    classifications: List[int]
+
+
+def create_sample(
+    methods: List[Any], positives: List[str], negatives: List[str]
+) -> Sample:
+    samples = []
+    classifications = []
+
+    def update_samples(documents: List[str], num: int):
+        nonlocal samples
+        nonlocal classifications
+        nonlocal methods
+        for document in documents:
+            classifications.append(num)
+            add = []
+            for fn in methods:
+                add.append(fn(document))
+            samples.append(add)
+
+    update_samples(positives, 1)
+    update_samples(negatives, -1)
+    return Sample(samples, classifications)
+
+
+@dataclass
+class Heuristic:
+    name: str
+    method: List[Any]
+    train: Sample
+    test: Sample
+
+    def __init__(
+        self, name: str, fns: List[Any], positives: List[str], negatives: List[str]
+    ):
+        self.name = name
+        training_positives = positives[:TRAINING_SIZE]
+        training_negatives = negatives[:TRAINING_SIZE]
+        self.train = create_sample(fns, training_positives, training_negatives)
+        max_positives = positives[TRAINING_SIZE:]
+        max_negatives = negatives[TRAINING_SIZE:]
+        max_len = min(len(max_positives), len(max_negatives))
+        testing_positives = max_positives[:max_len]
+        testing_negatives = max_negatives[:max_len]
+        self.test = create_sample(fns, testing_positives, testing_negatives)
+        self.method = fns
+
+
+@dataclass
+class Dataset:
+    name: str
+    slang: Heuristic
+    antonyms: Heuristic
+    sounds: Heuristic
+    all: Heuristic
+
+    def __init__(self, name: str, positives: List[str], negatives: List[str]):
+        self.name = name
+        self.slang = Heuristic(
+            f"{name} slang", [count_adult_slang], positives, negatives
+        )
+        self.sounds = Heuristic(f"{name} sounds", [count_sounds], positives, negatives)
+        self.antonyms = Heuristic(
+            f"{name} antonyms", [count_antonyms], positives, negatives
+        )
+        self.all = Heuristic(
+            f"{name} all",
+            [count_adult_slang, count_sounds, count_antonyms],
+            positives,
+            negatives,
+        )
--- code/learn_heuristics.py
+++ code/learn_heuristics.py
@@ -1,38 +1,74 @@
-#!/usr/bin/env python3
-from dataclasses import dataclass
+print("Installing imports...")
+from sklearn.tree import DecisionTreeClassifier
 from typing import List
-from sklearn import tree
-from datasets import DATA_MAP
+from gen_heuristic_params import Dataset
+from scrape import (
+    extract_jokes,
+    extract_jeopardy_questions,
+    extract_news_headlines,
+    extract_reviews,
+)
+
+print("Extracting jokes from CSV...")
+JOKES = extract_jokes()
+
+print("Extracting jeopardy from CSV...")
+JEOPARDY = extract_jeopardy_questions()
+
+print("Extracting news from CSV...")
+NEWS = extract_news_headlines()
+
+print("Extracting reviews from CSV...")
+REVIEWS = extract_reviews()
 
 
-@dataclass
-class ModelParameters:
-    heuristic: str
-    training_set: str
-    samples: List[float]
-    features: List[int]
+def create_dataset(name: str, negatives: List[str]) -> Dataset:
+    return Dataset(name, JOKES, negatives)
 
 
-def extract_model_parameters() -> List[ModelParameters]:
-    models: List[ModelParameters] = []
-    FEATURES = []
-    with open("raw/computed_params") as param_file:
-        for line in param_file:
-            elems = [el.strip() for el in line.split("\t")]
-            if elems[0] == "classifications":
-                FEATURES = eval(elems[1])
-                continue
-            heuristic = elems[0]
-            training_set = elems[1]
-            training_parameters = eval(elems[2])
-            model = ModelParameters(
-                heuristic, training_set, training_parameters, FEATURES
-            )
-            models.append(model)
-    return models
+def evaluate(name: str, negatives: List[str]):
+    print(f"Creating dataset for {name}...")
+    dataset = create_dataset(name, negatives)
+
+    print(f"Evaluating {name} slang...")
+    slang_classifier = DecisionTreeClassifier()
+    slang_classifier.fit(
+        dataset.slang.train.samples, dataset.slang.train.classifications
+    )
+    score = slang_classifier.score(
+        dataset.slang.test.samples, dataset.slang.test.classifications
+    )
+    print(f"Score: {score}")
+
+    print(f"Evaluating {name} sounds...")
+    sounds_classifier = DecisionTreeClassifier()
+    sounds_classifier.fit(
+        dataset.sounds.train.samples, dataset.sounds.train.classifications
+    )
+    score = sounds_classifier.score(
+        dataset.sounds.test.samples, dataset.sounds.test.classifications
+    )
+    print(f"Score: {score}")
+
+    print(f"Evaluating {name} antonyns...")
+    antonym_classifier = DecisionTreeClassifier()
+    antonym_classifier.fit(
+        dataset.antonyms.train.samples, dataset.antonyms.train.classifications
+    )
+    score = antonym_classifier.score(
+        dataset.antonyms.test.samples, dataset.antonyms.test.classifications
+    )
+    print(f"Score: {score}")
+
+    print(f"Evaluating {name} all...")
+    all_classifier = DecisionTreeClassifier()
+    all_classifier.fit(dataset.all.train.samples, dataset.all.train.classifications)
+    score = all_classifier.score(
+        dataset.all.test.samples, dataset.all.test.classifications
+    )
+    print(f"Score: {score}")
 
 
-# TODO: Write a function that iterates over the model parameters and then
-# evaluates the performance on the test data. Once we get this working
-# for the heuristic decision trees, replicate with Naive Bayes and the SMV
-# vectors.
+evaluate("reviews", REVIEWS)
+evaluate("jeopardy", JEOPARDY)
+evaluate("news", NEWS)
