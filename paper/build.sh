b14df97b31899296359cfb96d917c8ddf1b69084
joshuaharry <84721070+joshuaharry@users.noreply.github.com>
2021-12-01 15:20:54 -0600

    Final paper tweaks

--- paper/build.sh
+++ paper/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "$1" ]; then
-  bibtex paper && pdflatex -halt-on-error  ./paper.tex;
+  pdflatex -halt-on-error  ./paper.tex;
 else
   watchexec -e tex -- "bibtex paper && pdflatex -halt-on-error  ./paper.tex"
 fi
